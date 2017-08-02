314c1fd1ea0b588ab94934bc7b98432e3990ef96
mqwu <send2mqwu@gmail.com>
2017-08-02 16:30:44 -0500

    Crack price correlation study

--- code/tic.R
+++ code/tic.R
@@ -118,6 +118,11 @@ bb.y.crude.sum <- bb.y.crude %>% group_by(date) %>% summarise(min_RB=min(RB), ma
 d <- read.csv("./data/tic/crude_gas.csv", header=T, na.strings = "NA")
 d$date <- as.Date(d$date)
 
+# create indicator before and after EIA release
+d <- d %>% 
+          mutate(hr=hour(mdy_hm(d$time)), min=minute(mdy_hm(d$time))) %>% 
+          mutate(aftEIA=ifelse(hr==11 | min>30, 1, 0))
+
 tic <- d %>% 
             group_by(date) %>% 
             summarise(RB_Mean = mean(RB),
@@ -126,12 +131,87 @@ tic <- d %>%
                       GCL_Mean = mean(GCL)
                      )
 
+
+# calculate crack value
+crack <- d %>% mutate(Crack_RB_BRN=RB*42-BRN,
+                      Crack_HO_BRN=HO*42-BRN,
+                      Crack_RB_GCL=RB*42-GCL,
+                      Crack_HO_GCL=HO*42-GCL
+                      )
+
+crack.sum <- crack %>% 
+                      group_by(date, aftEIA) %>% 
+                      summarise(
+                        mean_crack_RB_BRN = mean(Crack_RB_BRN),
+                        mean_crack_HO_BRN = mean(Crack_HO_BRN),
+                        max_crack_RB_BRN = max(Crack_RB_BRN),
+                        max_crack_HO_BRN = max(Crack_HO_BRN),
+                        min_crack_RB_BRN = min(Crack_RB_BRN),
+                        min_crack_HO_BRN = min(Crack_HO_BRN)
+                      )
+
+crack.aft <- crack.sum %>% 
+                          filter(aftEIA==1) %>% 
+                          rename(
+                                 aft_mean_crack_RB_BRN=mean_crack_RB_BRN, 
+                                 aft_mean_crack_HO_BRN=mean_crack_HO_BRN,
+                                 aft_max_crack_RB_BRN=max_crack_RB_BRN,
+                                 aft_max_crack_HO_BRN=max_crack_HO_BRN,
+                                 aft_min_crack_RB_BRN=min_crack_RB_BRN,
+                                 aft_min_crack_HO_BRN=min_crack_HO_BRN
+                          )
+
+crack.bf <- crack.sum %>% 
+                         filter(aftEIA==0) %>% 
+                         rename(
+                                bf_mean_crack_RB_BRN=mean_crack_RB_BRN, 
+                                bf_mean_crack_HO_BRN=mean_crack_HO_BRN,
+                                bf_max_crack_RB_BRN=max_crack_RB_BRN,
+                                bf_max_crack_HO_BRN=max_crack_HO_BRN,
+                                bf_min_crack_RB_BRN=min_crack_RB_BRN,
+                                bf_min_crack_HO_BRN=min_crack_HO_BRN
+                         )
+
+crack.1m.bf.eia <- crack %>% 
+                            filter(hr==10, min==29) %>% 
+                            select(date, time, Crack_RB_BRN, Crack_HO_BRN)
+
+crack.sum2 <- left_join(crack.aft, crack.bf, by="date") %>% select(-contains("aftEIA"))
+crack.sum3 <- left_join(crack.sum2, crack.1m.bf.eia, by="date")
+
+crack.sum4 <- crack.sum3 %>% 
+                          transmute(
+                                    diff_mean_crack_gas=aft_mean_crack_RB_BRN - bf_mean_crack_RB_BRN,
+                                    diff_mean_crack_dis=aft_mean_crack_HO_BRN - bf_mean_crack_HO_BRN,
+                                    
+                                    diff_mean_pct_crack_gas=(aft_mean_crack_RB_BRN - bf_mean_crack_RB_BRN)/bf_mean_crack_RB_BRN,
+                                    diff_mean_pct_crack_dis=(aft_mean_crack_HO_BRN - bf_mean_crack_HO_BRN)/bf_mean_crack_HO_BRN,
+                                    
+                                    max_to_bf_EIA_crack_gas=aft_max_crack_RB_BRN/Crack_RB_BRN,
+                                    max_to_bf_EIA_crack_dis=aft_max_crack_HO_BRN/Crack_HO_BRN,
+                                    
+                                    min_to_bf_EIA_crack_gas=aft_min_crack_RB_BRN/Crack_RB_BRN,
+                                    min_to_bf_EIA_crack_dis=aft_min_crack_HO_BRN/Crack_HO_BRN,
+                                    
+                                    diff_max_to_bf_EIA_crack_gas=aft_max_crack_RB_BRN-Crack_RB_BRN,
+                                    diff_max_to_bf_EIA_crack_dis=aft_max_crack_HO_BRN-Crack_HO_BRN,
+                                    
+                                    diff_min_to_bf_EIA_crack_gas=aft_min_crack_RB_BRN-Crack_RB_BRN,
+                                    diff_min_to_bf_EIA_crack_dis=aft_min_crack_HO_BRN-Crack_HO_BRN,
+                                    
+                                    
+                                    volaty_gas = aft_max_crack_RB_BRN - aft_min_crack_RB_BRN,
+                                    volaty_dis = aft_max_crack_HO_BRN - aft_min_crack_HO_BRN
+                                    )
+
+
+
 # load master card data
-ma <- read.csv("./data/mas/gas_shell-mas.csv", header=T, na.strings = "NA")
-ma$lastwkd <- as.Date(ma$lastwkd)
-ma$date <- ma$lastwkd + 5  # next wed, i.e. EIA release date
-keep <- c("date", "retail_gas", "Master_gas")
-ma <- ma[, keep]
+# ma <- read.csv("./data/mas/gas_shell-mas.csv", header=T, na.strings = "NA")
+# ma$lastwkd <- as.Date(ma$lastwkd)
+# ma$date <- ma$lastwkd + 5  # next wed, i.e. EIA release date
+# keep <- c("date", "retail_gas", "Master_gas")
+# ma <- ma[, keep]
 
 
 
@@ -143,39 +223,58 @@ keep <- c("lastwkd",
           "weekly_regular", "weekly_mid_grade", "weekly_svpn", "weekly_diesel",
           "Diesel", "Gas", "gas_Mean", "diesel_Mean")
 retail <- allin1[, keep]
+retail <- retail %>% mutate(
+                            diff_eia_to_bb_gas=Gas-gas_Mean,
+                            diff_eia_to_bb_dis=Diesel-diesel_Mean,
+                                
+                            abs_diff_eia_to_bb_gas=abs(Gas-gas_Mean),
+                            abs_diff_eia_to_bb_dis=abs(Diesel-diesel_Mean)
+                            )
 
 retail$date <- retail$lastwkd + 5  # next wed, i.e. EIA release date
 
+# crack: join all data: retail, crack
+allcrack <- left_join(retail, crack.sum4, by="date")
+
+
 
 # join all data: retail, master, tic data
-all <- left_join(retail, ma, by="date")
-all <- left_join(all, tic, by="date")
+# all <- left_join(retail, ma, by="date")
+# all <- left_join(all, tic, by="date")
+
+
+
 
 
 # corr
-A <- all[,-c(1, 10)]
+#A <- all[,-c(1, 10)]
+A <- allcrack[,-c(1, 14)]
 M <- cor(A, use="pairwise.complete.obs")
 
-png(paste0("./doc/fig/corr/corr_retail_ma_tic","_circle.png"), width=1200, height=1200)
+#png(paste0("./doc/fig/corr/corr_retail_ma_tic","_circle.png"), width=1200, height=1200)
+png(paste0("./doc/fig/corr/corr_crack_retail","_circle.png"), width=1000, height=1000)
 corrplot(M, method="circle", type="upper", col=brewer.pal(n=11, name="RdYlBu"))
 dev.off()
 
-png(paste0("./doc/fig/corr/corr_retail_ma_tic", "_num.png"), width=1200, height=1200)
+#png(paste0("./doc/fig/corr/corr_retail_ma_tic", "_num.png"), width=1200, height=1200)
+png(paste0("./doc/fig/corr/corr_crack_retail", "_num.png"), width=1000, height=1000)
 corrplot(M, method="number", type="upper", col=brewer.pal(n=8, name="RdYlBu"))
 dev.off()
 
           
     
-all16to17 <- all %>% filter(year(date)>2015)
+#all16to17 <- all %>% filter(year(date)>2015)
+all16to17 <- allcrack %>% filter(year(date)>2015)
 # corr
-A <- all16to17[,-c(1, 10)]
+#A <- all16to17[,-c(1, 10)]
+A <- all16to17[,-c(1, 14)]
 M <- cor(A, use="pairwise.complete.obs")
 
-png(paste0("./doc/fig/corr/corr_16to17_retail_ma_tic","_circle.png"), width=1200, height=1200)
+png(paste0("./doc/fig/corr/corr_16to17_crack_retail","_circle.png"), width=1300, height=1300)
 corrplot(M, method="circle", type="upper", col=brewer.pal(n=11, name="RdYlBu"))
 dev.off()
 
-png(paste0("./doc/fig/corr/corr_16to17_retail_ma_tic", "_num.png"), width=1200, height=1200)
+png(paste0("./doc/fig/corr/corr_16to17_crack_retail", "_num.png"), width=1300, height=1300)
 corrplot(M, method="number", type="upper", col=brewer.pal(n=8, name="RdYlBu"))
 dev.off()
 
